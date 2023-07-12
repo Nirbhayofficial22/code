930ebbd8116bd136839585be60f6a2dfba7f40ef
Nirbhay Kumar Gupta <nirbhay.official22@gmail.com>
2023-07-12 14:52:03 +0530

    Added main class

---
+++ Eclipse/Calculator/src/com/home/calci/Main.java
@@ -0,0 +1,8 @@
+package com.home.calci;
+
+public class Main {
+	public static void main(String [] args) {
+		//making program for calculator.
+		System.out.print("Lets Start the coding journey");
+	}
+}
